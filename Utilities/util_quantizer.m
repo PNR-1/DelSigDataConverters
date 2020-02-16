2d08df0b8d0bbd981be074ad4cb43c123305e5a3
Preetham Reddy <preethamreddy@Preethams-MacBook-Pro.local>
2020-02-16 20:11:32 +0530

    Fixed incorrect Quantiser outputs.

--- Utilities/util_quantizer.m
+++ Utilities/util_quantizer.m
@@ -13,21 +13,27 @@ function [y_out,overload,Qmin,Qmax] = util_quantizer(y_in,Delta,NumLevels,Type)
 %NumLevels  -> Number of levels of the Quantizer
 %Type       -> 'MidRise', 'MidTread'
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+y_in = -10:0.01:10;
+Delta = 1;
+NumLevels = 8;
+Type = 'MidRise';
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Creating Limits for Quantiser
 M = NumLevels - 1;
+correctionF = Delta/2;
 x_lowLimVariables = -(M+1)*(Delta/2):Delta:(M+1)*(Delta/2);
-if(strcmp(Type,'MidTread') && ~(ismember(0,x_lowLimVariables)))
-    x_lowLimVariables = x_lowLimVariables - Delta/2;
+if(strcmp(Type,'MidRise') && ~(ismember(0,x_lowLimVariables)))
+        x_lowLimVariables = x_lowLimVariables - Delta/2;
 end
 
-if(strcmp(Type,'MidRise') && ismember(0,x_lowLimVariables))
-    x_lowLimVariables = x_lowLimVariables - Delta/2;
+if(strcmp(Type,'MidTread') && ismember(0,x_lowLimVariables))
+        x_lowLimVariables = x_lowLimVariables - Delta/2;
 end
+
+
 tempVar = 0;
 
-y_out = (y_outputVar(1)*1e2)*ones(1,length(y_in));
+y_out = (x_lowLimVariables(1)*1e2)*ones(1,length(y_in));
 overload = zeros(1,length(y_in));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% START QUANTIZATION
@@ -36,15 +42,14 @@ Qmax = x_lowLimVariables(end);
 for i=1:length(y_in)
     if(y_in(i) >= x_lowLimVariables(end))
         overload(i) = 1;
-        y_out(i) = y_outputVar(end);
+        y_out(i) = x_lowLimVariables(end-1)+correctionF;
     elseif(y_in(i) <= x_lowLimVariables(1))
             overload(i) = 1;
-            y_out(i) = y_outputVar(1);
+            y_out(i) = x_lowLimVariables(1)+correctionF;
     else
         tempVar = floor( (y_in(i) - x_lowLimVariables(1))/Delta) + 1;
-        y_out(i) = y_outputVar(tempVar);
+        y_out(i) = x_lowLimVariables(tempVar)+correctionF;
     end  %if   
 end %end for
 clear tempVar;
-
 end% END FUNCTION
